82d5c44f2e753e0e22731124aefd0d8bc19c083b
jingahyunn <jingahyun@ewha.ac.kr>
2026-09-07 03:13:15 +0000

    lab1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,34 @@
-//staging
-//commit
-//push
+#include <iostream>
+
+int main()
+{
+    int score();
+    char grade();
+    std::cout << Enter score;
+    std::cin >> score;
+    switch(score/10);
+
+    {
+        case 10;
+        case 9: 
+    }
+
+
+
+
+    // std::cout << numofstudents << std:: endl;
+    // std::cout << sizeof(numofstudents)<< std:: endl;
+    // std::cout << typeid(numofstudents).name() << std::endl;
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
